af60d769aaf176542d754ace73e0efc0fbe339b9
Ramon Lucio <ramonluciolinhares@gmail.com>
2021-12-16 20:14:19 +0100

    feat: add struct to store images

--- so_long.h
+++ so_long.h
@@ -6,22 +6,29 @@
 /*   By: rlucio-l <rlucio-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 13:11:54 by rlucio-l          #+#    #+#             */
-/*   Updated: 2021/12/16 13:17:18 by rlucio-l         ###   ########.fr       */
+/*   Updated: 2021/12/16 17:34:38 by rlucio-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
-
 # include <mlx.h>
 # include <X11/keysym.h>
 # include <X11/X.h>
 # include <stdlib.h>
 
+typedef struct s_img
+{
+	void	*img;
+	int		width;
+	int		height;
+}				t_img;
+
 typedef struct s_ptr
 {
 	void	*mlx;
 	void	*win;
+	t_img	tile;
 }				t_ptr;
 
 #endif
