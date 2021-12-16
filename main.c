1af69da6fdfac59905ffca5b72d320df7191a8ed
Ramon Lucio <ramonluciolinhares@gmail.com>
2021-12-16 20:07:35 +0100

    feat: put xpm_file to image and put image to window
    The management of your window must remain smooth: changing to another window, minimizing, etc.
    
    Function handle_no_event() called by mlx_loop_hook() put image to window and handle window management

--- main.c
+++ main.c
@@ -6,43 +6,47 @@
 /*   By: rlucio-l <rlucio-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 22:55:54 by rlucio-l          #+#    #+#             */
-/*   Updated: 2021/12/15 18:14:57 by rlucio-l         ###   ########.fr       */
+/*   Updated: 2021/12/16 19:56:35 by rlucio-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
-#include <stdio.h>
-#include <X11/keysym.h>
-#include <stdlib.h>
-#include <X11/X.h>
+#include "so_long.h"
 
-typedef struct s_ptr
+int	handle_no_event(t_ptr *ptr_to)
 {
-	void	*mlx;
-	void	*win;
-}				t_ptr;
+	int	i;
+	int	j;
 
-/*	mlx_loop_hook(ptr_to.mlx, &handle_no_event, &ptr_to) will be called when
-	no event occurs. The only reason we've used it is because without it,
-	the loop would have never ended. That's directly related to how the
-	mlx_loop is implemented. */
-
-int	handle_no_event(void)
-{
+	i = 0;
+	j = 0;
+	ptr_to->tile.img = mlx_xpm_file_to_image(ptr_to->mlx, "assets/tile.xpm",
+			&ptr_to->tile.width, &ptr_to->tile.height);
+	while (j < 512)
+	{
+		while (i < 512)
+		{
+			mlx_put_image_to_window(
+				ptr_to->mlx, ptr_to->win, ptr_to->tile.img, i, j);
+			i += 16;
+		}
+		i = 0;
+		j += 16;
+	}
+	mlx_destroy_image(ptr_to->mlx, ptr_to->tile.img);
 	return (0);
 }
-
-/*	Pressing ESC must close the window and quit the program cleanly.*/
 
 int	escape_window(int keycode, t_ptr *ptr_to)
 {
 	if (keycode == XK_Escape)
+	{
 		mlx_destroy_window(ptr_to->mlx, ptr_to->win);
+		mlx_destroy_display(ptr_to->mlx);
+		free(ptr_to->mlx);
+		exit(0);
+	}
 	return (0);
 }
-
-/*	Clicking on the red cross on the window’s frame must close the window and
-	quit the program cleanly. */
 
 int	close_window(t_ptr *ptr_to)
 {
@@ -60,7 +64,7 @@ int	main(void)
 	ptr_to.mlx = mlx_init();
 	if (ptr_to.mlx == NULL)
 		return (1);
-	ptr_to.win = mlx_new_window(ptr_to.mlx, 500, 500, "Pressing esc");
+	ptr_to.win = mlx_new_window(ptr_to.mlx, 512, 512, "./so_long");
 	if (ptr_to.win == NULL)
 	{
 		free(ptr_to.win);
@@ -71,7 +75,5 @@ int	main(void)
 	mlx_hook(ptr_to.win, DestroyNotify, StructureNotifyMask, &close_window,
 		&ptr_to);
 	mlx_loop(ptr_to.mlx);
-	mlx_destroy_display(ptr_to.mlx);
-	free(ptr_to.mlx);
 	return (0);
 }
